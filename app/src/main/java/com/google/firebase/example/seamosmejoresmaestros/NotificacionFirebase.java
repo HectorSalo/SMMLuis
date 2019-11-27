adb1024e0dbe5810854b01f393852f87a6964a37
Prueba <hectorsalomonchirinos@gmial.com>
2019-11-27 13:12:55 -0400

    V3.0.19

--- app/src/main/java/com/google/firebase/example/seamosmejoresmaestros/NotificacionFirebase.java
+++ app/src/main/java/com/google/firebase/example/seamosmejoresmaestros/NotificacionFirebase.java
@@ -3,18 +3,26 @@ package com.google.firebase.example.seamosmejoresmaestros;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
+import androidx.preference.PreferenceManager;
+
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.Random;
+
+import static android.graphics.Color.rgb;
 
 public class NotificacionFirebase extends FirebaseMessagingService {
 
@@ -23,41 +31,104 @@ public class NotificacionFirebase extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        SharedPreferences preferences = getSharedPreferences("notificacion", Context.MODE_PRIVATE);
-        boolean  permitir = preferences.getBoolean("activar", true);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean activarVibrar = sharedPreferences.getBoolean("activarVibrar", false);
 
-        if (permitir) {
+
             if (remoteMessage.getNotification() != null) {
-                Log.d("Contenido", remoteMessage.getNotification().getBody());
-                mostrarNotif(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+                if (activarVibrar) {
+                    Log.d("MSG", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+                    mostrarNotif(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+                } else {
+                    Log.d("MSG", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+                    mostrarNotifNoVibrar(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+                }
             }
-        } else {
-            Log.d("Notificacion", "Desactivada");
-        }
+
     }
 
     private void mostrarNotif(String title, String body) {
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        //
         String NOTIFICATION_CHANNEL_ID = "MaestrosNotif";
 
+
+        NotificationCompat.Builder notificationBuilder =  new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder
+                .setSmallIcon(R.drawable.ic_notif)
+                .setColor(rgb(0, 60, 255))
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{0, 1000, 500, 1000})
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+
+                .setContentInfo("info");
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_DEFAULT);
-            notificationChannel.setDescription("VMC channel");
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setDescription("Descripcion");
             notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableLights(true);
             notificationManager.createNotificationChannel(notificationChannel);
         }
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_CHANNEL_ID);
 
-        builder.setAutoCancel(true)
-                .setWhen(System.currentTimeMillis())
-                .setDefaults(Notification.DEFAULT_ALL)
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+    }
+
+    private void mostrarNotifNoVibrar(String title, String body) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        //
+        String NOTIFICATION_CHANNEL_ID = "MaestrosNotif";
+
+
+        NotificationCompat.Builder notificationBuilder =  new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder
                 .setSmallIcon(R.drawable.ic_notif)
+                .setColor(rgb(0, 60, 255))
                 .setContentTitle(title)
-                .setContentText(body);
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setVibrate(null)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
 
-        notificationManager.notify(new Random().nextInt(), builder.build());
+                .setContentInfo("info");
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setDescription("Descripcion");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(null);
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
     }
 
     @Override
